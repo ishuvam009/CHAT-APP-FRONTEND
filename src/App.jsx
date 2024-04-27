46e4e245c5fdc68f00e09ce380d26ded78d11a07
SHUVAM <120933772+ishuvam009@users.noreply.github.com>
2024-04-27 09:20:58 +0000

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -10,7 +14,7 @@ function App() {
 <button className="btn btn-secondary">Secondary</button>
 <button className="btn btn-accent">Accent</button>
 <button className="btn btn-ghost">Ghost</button>
-<button className="btn btn-link">Link</button>
+<button className="btn btn-link">Link</button>  
     </>
   )
 }
